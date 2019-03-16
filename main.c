2ad037e5e5947ed0dc8152882dd76fac6ed555b3
Skrezhendevsky <alekskrzh@gmail.com>
2019-03-16 19:38:01 +0300

    ver 1.0

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #define M 3 //lines
 #define N 4 //
 
+#define ACCURASY 0.00001 //
+
 void printMatrix(int m, int n,float* matrix) 
 {
 	int i, j;
@@ -37,96 +39,200 @@ void SumMatrixLine(int m, int n, float* matrix, float k1, float k2, int indx1, i
 	}
 }
 
-int main(void) 
+void MovMatrixLine(int m, int n, float* matrix, int indx1, int indx2) // 2 - 1 
 {
-    int i, j, maxIndex;
-	float matrix[M][N] = {0};
+	int i, j;
+	float buff;
+	for(j = 0; j < n; j++)
+	{
+		buff = *(matrix + (m+1)*indx2 + j);
+		*(matrix + (m+1)*indx2 + j) = (*(matrix + (m+1)*indx1 + j));
+		(*(matrix + (m+1)*indx1 + j)) = buff;
+	}
+}
+
+void SimpleIterationMode()
+{
+	 int i, j, maxIndex;
+	float matrix[M][N] = {};
+	float x[M] = {};
+	float oldx[M] = {};
     float max, buff;
-    matrix[0][0] = 0.14; 	matrix[0][1] = 0.24; 	matrix[0][2] = -0.84; 	matrix[0][3] = 1.11; //0.14*x + 0.24*y - 0.84*z = 1.11
-	matrix[1][0] = 1.07; 	matrix[1][1] = -0.83; 	matrix[1][2] = 0.56; 	matrix[1][3] = 0.48; //1.07*x - 0.83 + 0.56*z = -0.84
-	matrix[2][0] = 0.64; 	matrix[2][1] = 0.43;	matrix[2][2] = -0.38;	matrix[2][3] = -0.83; //0.64*x + 0.43*y - 0.38*z = 0.64
-    printMatrix (M,N,(float*) matrix);
+    
+    matrix[0][0] = 0.12; 	matrix[0][1] = -0.43; 	matrix[0][2] = 0.14; 	matrix[0][3] = -0.17; //0.14*x + 0.24*y - 0.84*z = 1.11
+	matrix[1][0] = -0.07; 	matrix[1][1] = 0.34; 	matrix[1][2] = 0.72; 	matrix[1][3] = 0.62; //1.07*x - 0.83 + 0.56*z = -0.84
+	matrix[2][0] = 1.18; 	matrix[2][1] = -0.08;	matrix[2][2] = -0.25;	matrix[2][3] = 1.12; //0.64*x + 0.43*y - 0.38*z = 0.64
 
     for(i = 0; i < M; i++)
 	{
-		for(j = 0; j < N; j++)
+		for(j = 0; j < N - 1; j++)
         {
-            if(j = 0)
+            if(j == 0)
             {
                 max = matrix[i][0];
+            	maxIndex = 0;
                 continue;
             }
-            if(max < matrix[i][j])
+            if(fabs(max) < fabs(matrix[i][j]))
             {
                 max = matrix[i][j];
                 maxIndex = j;
             }
         }
-        if(maxIndex = i)
+        if(i == maxIndex)
         {
-            continue;
-        }
-        buff = matrix[i][i];
-        matrix[i][i] = matrix[i][maxIndex];
-        matrix[i][maxIndex] = buff;
+        	continue;
+		}
+		else
+		{
+			MovMatrixLine(M,N,(float*) matrix,i,maxIndex);
+		}
 	}
-
-    printMatrix (M,N,(float*) matrix);
 
     for(i = 0; i < M; i++)
 	{
-		modMatrixLine(M, N, (float*) matrix, 1.0/matrix[i][i], i);
+		modMatrixLine(M, N, (float*) matrix, -1.0/matrix[i][i], i);
+		matrix[i][i] = 0;
 	}
-
-    printMatrix (M,N,(float*) matrix);
-
-    /*
-	int i, j;
-	float matrix[M][N] = {0};
-	
-//	matrix[0][0] = 1; 	matrix[0][1] = 1; 	matrix[0][2] = 2;
-//	matrix[1][0] = 2; 	matrix[1][1] = 4; 	matrix[1][2] = 6;
-
-	matrix[0][0] = 0.14; 	matrix[0][1] = 0.24; 	matrix[0][2] = -0.84; 	matrix[0][3] = 1.11; //0.14*x + 0.24*y - 0.84*z = 1.11
-	matrix[1][0] = 1.07; 	matrix[1][1] = -0.83; 	matrix[1][2] = 0.56; 	matrix[1][3] = 0.48; //1.07*x - 0.83 + 0.56*z = -0.84
-	matrix[2][0] = 0.64; 	matrix[2][1] = 0.43;	matrix[2][2] = -0.38;	matrix[2][3] = -0.83; //0.64*x + 0.43*y - 0.38*z = 0.64
 	
 	
-	printMatrix (M,N,(float*) matrix);
-	
-	for(j = 0; j < N-1; j++)
+	for(i = 0; i < M; i++)
 	{
-		modMatrixLine(M, N, (float*) matrix, 1.0/matrix[j][j], j);
-		printMatrix (M,N,(float*) matrix);
-		for(i = 0; i < M - 1 - j; i++)
-		{
-			SumMatrixLine(M, N, (float*) matrix, - matrix[i+1+j][j], 1.0, j, i+1+j);
-			printMatrix (M,N,(float*) matrix);
+		x[i] = 0;
+		oldx[i] = 0;
+	}
+	
+    float delta;
+    int k = 0;
+    do
+    {
+    	for(i = 0; i < M; i++)
+    	{
+    		oldx[i] = x[i];
 		}
-	}
-	
-	float x[M] = {0};
-	
-	for(i = (M - 1); i >= 0; i--)
-	{
-		x[i] = matrix[i][N-1];
-	//	printf("%f\n", x[i]);
-		for(j = 0; j < N - 2 - i; j++)//j = i; j < N - 2; j++)
-		{
-			if(i == M-1)
-			{
-				continue;
+    	for(i = 0; i < M; i++)
+    	{
+    		x[i] = 0;
+    		for(j = 0; j < M; j++)
+    		{
+    			x[i] += matrix[i][j] * oldx[j];
 			}
-		//	printf("%f,  %f\n", x[i+j+1],matrix[i][i+j+1]);
-			x[i] = x[i] - x[i+j+1]*matrix[i][i+j+1];
+			x[i] -= matrix[i][N-1];
 		}
-	}	
-	
-	printf("\n");
-	printf("\n");
+		
+		
+		delta = fabs(x[0] - oldx[0]);
+		for(i = 1; i < M; i++)
+		{
+			if(delta < fabs(x[i] - oldx[i]))
+			{
+				delta = fabs(x[i] - oldx[i]);
+			}
+		}
+		k++;
+	}
+	while((fabs(delta) > ACCURASY));
+//	printf("iteration counter = %d\n",k);
 	printMatrix(1,M,x);
+}
 
+void ZeydelIterationMode()
+{
+	 int i, j, maxIndex;
+	float matrix[M][N] = {};
+	float x[M] = {};
+	float oldx[M] = {};
+    float max, buff;
+    
+    matrix[0][0] = 0.12; 	matrix[0][1] = -0.43; 	matrix[0][2] = 0.14; 	matrix[0][3] = -0.17; //0.14*x + 0.24*y - 0.84*z = 1.11
+	matrix[1][0] = -0.07; 	matrix[1][1] = 0.34; 	matrix[1][2] = 0.72; 	matrix[1][3] = 0.62; //1.07*x - 0.83 + 0.56*z = -0.84
+	matrix[2][0] = 1.18; 	matrix[2][1] = -0.08;	matrix[2][2] = -0.25;	matrix[2][3] = 1.12; //0.64*x + 0.43*y - 0.38*z = 0.64
+
+    for(i = 0; i < M; i++)
+	{
+		for(j = 0; j < N - 1; j++)
+        {
+            if(j == 0)
+            {
+                max = matrix[i][0];
+            	maxIndex = 0;
+                continue;
+            }
+            if(fabs(max) < fabs(matrix[i][j]))
+            {
+                max = matrix[i][j];
+                maxIndex = j;
+            }
+        }
+        if(i == maxIndex)
+        {
+        	continue;
+		}
+		else
+		{
+			MovMatrixLine(M,N,(float*) matrix,i,maxIndex);
+		}
+	}
+
+    for(i = 0; i < M; i++)
+	{
+		modMatrixLine(M, N, (float*) matrix, -1.0/matrix[i][i], i);
+		matrix[i][i] = 0;
+	}
 	
-	*/
+	
+	for(i = 0; i < M; i++)
+	{
+		x[i] = 0;
+		oldx[i] = 0;
+	}
+	
+    float delta;
+    int k = 0;
+    do
+    {
+    	for(i = 0; i < M; i++)
+    	{
+    		oldx[i] = x[i];	
+		}
+    	for(i = 0; i < M; i++)
+    	{
+    		x[i] = 0;
+    		for(j = 0; j < M; j++)
+    		{
+    			x[i] += matrix[i][j] * oldx[j];
+			}
+			x[i] -= matrix[i][N-1];
+			oldx[i] = x[i];
+		}
+		
+		
+		delta = fabs(x[0] - oldx[0]);
+		for(i = 1; i < M; i++)
+		{
+			if(delta < fabs(x[i] - oldx[i]))
+			{
+				delta = fabs(x[i] - oldx[i]);
+			}
+		}
+		k++;
+	}
+	while((fabs(delta) > ACCURASY));
+//	printf("iteration counter = %d\n",k);
+	printMatrix(1,M,x);
+}
+
+int main(void) 
+{
+   	printf("Reshit sistemy:\n1 - metodom postyh iteraciy\n2 - metodom Zeydelya\n");
+   	char choice = getch();
+ 	if(choice = '1')
+ 	{
+ 		SimpleIterationMode();
+	}
+	else
+	{
+		ZeydelIterationMode();
+	}
 	return 0;
 }
